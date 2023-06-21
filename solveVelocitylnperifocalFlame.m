e3b7a4f16f8551914faf1a7e58f4726d1f0b9d1c
YTS <YTS@172.16.36.73>
2023-06-22 04:25:24 +0900

    hw(modify)

--- solveVelocitylnperifocalFlame.m
+++ solveVelocitylnperifocalFlame.m
@@ -1,6 +1,6 @@
 function velocitylnPQW=solveVelocitylnperifocalFlame(a,e,v)
 %a:semimajor_axis, e=eccentricity, v:true_anomaly
-mu=398600;%km^3/s^2
+mu=3.986004418e+5;%km^3/s^2
 v=v*(pi/180);
 velocitylnPQW=sqrt(mu/(a*(1-e^2)))*[-sin(v) e+cos(v) 0]';
 end
