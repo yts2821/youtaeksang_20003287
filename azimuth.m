78c1957ac500e4a819e93dbc6d690592a82ebe91
YTS <YTS@172.16.36.73>
2023-06-22 21:43:20 +0900

    hw(modify)

--- azimuth.m
+++ azimuth.m
@@ -1,10 +1,10 @@
 function az=azimuth(ENU)
-az=atan2d(ENU(:,1),ENU(:,2));
+az=atan2(ENU(:,1),ENU(:,2));
 [m,n]=size(az);
 for i=1:1:m
     if (az(i)<0)
         az(i)=az(i)+2*pi;
     end
 end
-az=az';
+az=180/pi*az'
 end
